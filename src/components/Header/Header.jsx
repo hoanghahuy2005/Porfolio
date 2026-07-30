63db391953772be33b41147f5fbb73a5c5f2e1a7
PoPhechBoy <hoanghahuy225@gmail.com>
2026-07-30 20:36:17 +0700

    feat: add hero and header components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css"
+export default function Header(){
+    return(
+       <>
+        <header className="header">
+            <nav className="navigation">
+                <a>Trang chủ</a>
+                <a>Giới thiệu</a>
+                <a>Dự án</a>
+                <a>Liên hệ</a>
+            </nav>
+        </header>
+       </> 
+    )
+}
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import "./Hero.css"
+export default function Hero(){
+    return (
+        <section className="hero">
+            <div className="hero-container">
+                <div className="hero-content">
+                    <p id="txt"><span>✦</span> AVAILABLE FOR WORK</p>
+                    <h1 className="hero-title">
+                        <span>Thermal</span>
+                        <span>Engineer</span>
+                    </h1>
+                    <div className="description">
+                        <p>I specialize in HVAC system design, heat load calculations, ventilation,
+        air-conditioning systems, and energy-efficient engineering solutions.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
